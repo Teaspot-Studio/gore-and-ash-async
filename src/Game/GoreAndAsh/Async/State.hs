752cb3ecb53f4273b7eb1c3e38c316427b5a868c
NCrashed <NCrashed@gmail.com>
2016-01-27 15:15:48 +0300

    State description

--- src/Game/GoreAndAsh/Async/State.hs
+++ src/Game/GoreAndAsh/Async/State.hs
@@ -1,3 +1,34 @@
+{-|
+Module      : Game.GoreAndAsh.Async.State
+Description : Internal state of core module
+Copyright   : (c) Anton Gushcha, 2016
+License     : BSD3
+Maintainer  : ncrashed@gmail.com
+Stability   : experimental
+Portability : POSIX
+-}
 module Game.GoreAndAsh.Async.State(
-
+    AsyncState(..)
+  , emptyAsyncState
   ) where
+
+import Control.DeepSeq
+import GHC.Generics (Generic)
+
+-- | Internal state of asynchronious core module
+--
+-- [@s@] - state of next module, they are chained until bottom, that is usually
+--         an empty data type.
+data AsyncState s = AsyncState {
+  asyncNextState :: !s
+} deriving (Generic)
+
+instance NFData s => NFData (AsyncState s)
+
+-- | Create inital async state
+--
+-- [@s@] -  state of next module
+emptyAsyncState :: s -> AsyncState s 
+emptyAsyncState s = AsyncState {
+    asyncNextState = s
+  }

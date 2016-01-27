077c04ddad5fec62806bb244762966b19a35667b
NCrashed <NCrashed@gmail.com>
2016-01-27 15:35:30 +0300

    Monadic API stab

--- src/Game/GoreAndAsh/Async/API.hs
+++ src/Game/GoreAndAsh/Async/API.hs
@@ -1,3 +1,29 @@
+{-|
+Module      : Game.GoreAndAsh.Async.API
+Description : Monadic and arrow API for module
+Copyright   : (c) Anton Gushcha, 2016
+License     : BSD3
+Maintainer  : ncrashed@gmail.com
+Stability   : experimental
+Portability : POSIX
+-}
 module Game.GoreAndAsh.Async.API(
-
+    MonadAsync(..)
   ) where
+
+import Control.Monad.Trans 
+
+import Game.GoreAndAsh.Async.Module 
+
+-- | Low level monadic API for module.
+--
+-- Note: does not require 'm' to be 'IO' monad.
+class Monad m => MonadAsync m where 
+  -- | stab method, temporary
+  asyncStab :: m ()
+
+instance {-# OVERLAPPING #-} Monad m => MonadAsync (AsyncT s m) where
+  asyncStab = return ()
+
+instance {-# OVERLAPPABLE #-} (Monad (mt m), MonadAsync m, MonadTrans mt) => MonadAsync (mt m) where 
+  asyncStab = lift asyncStab
